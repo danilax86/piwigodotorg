3d724029474d7f723e1fb88ad274c33e617a0cb0
HWFord <lilypickle@hotmail.fr>
2021-09-02 13:37:31 +0200

    Testimonials if less than 15 testimonials, adds whats missing from English ones

--- include/testimonials.inc.php
+++ include/testimonials.inc.php
@@ -12,5 +12,20 @@ foreach ($testimonials as $testimonial) {
   }
 }
 
+if(count($lang_testimonials)<15){
+
+  $number_of_testimonials_to_add = 15 - count($lang_testimonials);
+  $count=0;
+  while(count($lang_testimonials)<16){
+    if ($testimonials[$count]['language'] != $lang_info['code'] && $testimonials[$count]['language'] == 'en') {
+      $testimonials[$count]['content'] = nl2br(trim($testimonials[$count]['content']));
+      $testimonials[$count]['added_on'] = format_date($testimonials[$count]['added_on'], array('month', 'year'));
+      $lang_testimonials[] = $testimonials[$count];
+    }
+    $count++;
+  }
+
+}
+
 $template->assign('testimonials', $lang_testimonials);
 ?>
